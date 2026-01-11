8e39723e082853398f1163dbbff4d5009a2f6b4a
ASHOT-Babaykin <212865037+MrAkakievic@users.noreply.github.com>
2026-01-11 08:21:47 -0500

    main-cycle added

--- inshallah.py
+++ inshallah.py
@@ -1,2 +1,9 @@
 
 import pygame, random, numpy as np, copy
+
+sc = pygame.display.set_mode((1600, 900))
+
+while True:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            exit()
